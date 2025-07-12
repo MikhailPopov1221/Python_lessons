a627c58c888cca2dbe47271edd14e7eaa000578b
Mikhail Popov <popov_muy@t.top-academy.ru>
2025-07-12 20:59:53 +0300

    magic methods

---
+++ july/12.07/1.py
@@ -0,0 +1,14 @@
+class Auto:
+    def __init__(self, mark):
+        self.mark = mark
+        
+    def __str__(self):
+        return f"Автомобиль марки {self.mark}"
+        
+        
+car = Auto("BMW")
+print(car.mark)
+print(car)
+
+# str(car)
+# repr(car)
---
+++ july/12.07/7.py
@@ -0,0 +1,6 @@
+x = 1
+
+print(+x)
+print(-x)
+print(~x)
+
